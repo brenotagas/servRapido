85a04510f0328c463a03afe55a3b66750d8d7a8e
Bruno Beltrao da Costa <bruno.costa@geap.com.br>
2017-10-20 15:57:30 -0200

    outro breno 3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var data = require('./breno.json');
 
 //... your code here ...
+
+app.get('/',(req,res)=>{
+    res.send(data);
+});
+
                                 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
